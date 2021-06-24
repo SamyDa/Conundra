1a941f779cc7ded74364424a5d7a6bcbc8334b69
samyd <samyd@192.168.1.62>
2021-06-24 11:59:24 +0200

    Refactoring without Conjured item

--- Java/Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/Java/src/main/java/com/gildedrose/GildedRose.java
@@ -1,67 +1,74 @@
 package com.gildedrose;
 
+import java.util.Arrays;
+
 class GildedRose {
     private static Item[] items;
 
-    public GildedRose(Item[] items) {
-        this.items = items;
-    }
+    private static final String AGED_BRIE = "Aged Brie";
+    private static final String BACKSTAGE = "Backstage passes to a TAFKAL80ETC concert";
+    private static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
+    
+    private static final String[] NOT_DECREASABLE_QUALITY_ITEMS = {AGED_BRIE, BACKSTAGE, SULFURAS};
     
     public static void setItems(Item[] newItems) {
     	items = newItems;
     }
     
-    
     public static void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
-                    }
-                }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
-
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-                    }
-                }
+    	for(Item item : items) {
+            if (!decreaseQuality(item)) {
+            	
+            	increaseQuality(item, false);
             }
-
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                items[i].sellIn = items[i].sellIn - 1;
+            
+            decreaseSellIn(item);
+            
+            if (item.sellIn < 0) {
+            
+	            switch(item.name) {
+		            case AGED_BRIE : increaseQuality(item, false);
+		            				 break;
+		            case BACKSTAGE : item.quality = 0;
+		            				 break;
+		            default : decreaseQuality(item);
+	            }
             }
+            
+        }
+    }
+    
+    private static void decreaseSellIn(Item item) {
+    	 if (!item.name.equals(SULFURAS)) {
+             item.sellIn = item.sellIn - 1;
+         }
+	}
 
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality;
-                    }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
-                    }
-                }
+	private static boolean decreaseQuality(Item item) {
+		
+		
+    	if (!Arrays.asList(NOT_DECREASABLE_QUALITY_ITEMS).contains(item.name) && item.quality > 0) {
+            item.quality = item.quality - 1;
+        }
+    	
+    	return !item.name.equals(AGED_BRIE) && !item.name.equals(BACKSTAGE);
+    }
+    
+    private static void increaseQuality(Item item, boolean isRecursiveCall) {
+    	
+    	
+    	if (item.quality < 50) {
+            item.quality = item.quality + 1;
+        }
+    	
+    	if (item.name.equals(BACKSTAGE ) && isRecursiveCall == false) {
+            if (item.sellIn < 11) {
+            	increaseQuality(item, true);
+            }
+            if (item.sellIn < 6) {
+            	increaseQuality(item, true);
             }
         }
     }
+    
 }
